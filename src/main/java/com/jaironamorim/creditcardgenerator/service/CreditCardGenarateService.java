27eabfbbcb34af11efe80efe6234d3d55935b13f
Jairon Amorim <jairon.amorim@grupomateus.com.br>
2023-01-22 01:07:56 -0300

    limpeza

--- src/main/java/com/jaironamorim/creditcardgenerator/service/CreditCardGenarateService.java
+++ src/main/java/com/jaironamorim/creditcardgenerator/service/CreditCardGenarateService.java
@@ -4,5 +4,6 @@ import com.jaironamorim.creditcardgenerator.model.dto.CreditCardDto;
 
 public interface CreditCardGenarateService {
     CreditCardDto generateCreditCard(String label, String checkdigit);
+
     boolean isValidCreditCardNumber(String creditCardNumber);
 }
