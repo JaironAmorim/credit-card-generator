1d34aa46fd91526daf99dc1d510468f3b25b2482
Jairon Amorim <jairon.amorim@grupomateus.com.br>
2023-01-27 00:20:15 -0300

    Melhoria no enum

--- src/main/java/com/jaironamorim/creditcardgenerator/model/enums/LabelEnum.java
+++ src/main/java/com/jaironamorim/creditcardgenerator/model/enums/LabelEnum.java
@@ -18,46 +18,27 @@ public enum LabelEnum {
     VOYAGER("VOYAGER");
 
     private final String label;
-    private static final List<String> VISA_PREFIX_LIST;
-    private static final List<String> MASTERCARD_PREFIX_LIST;
-    private static final List<String> AMEX_PREFIX_LIST;
-    private static final List<String> DISCOVER_PREFIX_LIST;
-    private static final List<String> DINERS_PREFIX_LIST;
-    private static final List<String> ENROUTE_PREFIX_LIST;
-    private static final List<String> JCB_16_PREFIX_LIST;
-    private static final List<String> JCB_15_PREFIX_LIST;
-    private static final List<String> VOYAGER_PREFIX_LIST;
+
+    private static final Map<LabelEnum, List<String>> prefixMap;
 
     LabelEnum(String label){
         this.label = label;
     }
 
     static {
-        VISA_PREFIX_LIST = Arrays.asList("4539", "4556", "4916", "4532", "4929", "40240071", "4485", "4716", "4");
-        MASTERCARD_PREFIX_LIST = Arrays.asList("51", "52", "53", "54", "55" );
-        AMEX_PREFIX_LIST = Arrays.asList( "34", "37");
-        DISCOVER_PREFIX_LIST = Collections.singletonList("6011");
-        DINERS_PREFIX_LIST = Arrays.asList("300", "301", "302", "303", "36", "38");
-        ENROUTE_PREFIX_LIST = Arrays.asList("2014", "2149");
-        JCB_16_PREFIX_LIST = Arrays.asList("3088", "3096", "3112", "3158", "3337", "3528");
-        JCB_15_PREFIX_LIST = Arrays.asList("2100", "1800");
-        VOYAGER_PREFIX_LIST = Collections.singletonList("8699");
-    }
 
-    private static Map<LabelEnum, List<String>> getMapLabel(){
-        EnumMap<LabelEnum, List<String>> labelMap = new EnumMap<>(LabelEnum.class);
-        labelMap.put(VISA, VISA_PREFIX_LIST);
-        labelMap.put(MASTER, MASTERCARD_PREFIX_LIST);
-        labelMap.put(AMEX, AMEX_PREFIX_LIST);
-        labelMap.put(DINERS, DINERS_PREFIX_LIST);
-        labelMap.put(DISCOVER, DISCOVER_PREFIX_LIST);
-        labelMap.put(ENROUTE, ENROUTE_PREFIX_LIST);
-        labelMap.put(JCB_16, JCB_16_PREFIX_LIST);
-        labelMap.put(JCB_15, JCB_15_PREFIX_LIST);
-        labelMap.put(VOYAGER, VOYAGER_PREFIX_LIST);
-        return labelMap;
-    }
+        prefixMap = new EnumMap<>(LabelEnum.class);
+        prefixMap.put(VISA, Arrays.asList("4539", "4556", "4916", "4532", "4929", "40240071", "4485", "4716", "4"));
+        prefixMap.put(MASTER, Arrays.asList("51", "52", "53", "54", "55" ));
+        prefixMap.put(AMEX, Arrays.asList( "34", "37"));
+        prefixMap.put(DISCOVER, Collections.singletonList("6011"));
+        prefixMap.put(DINERS, Arrays.asList("300", "301", "302", "303", "36", "38"));
+        prefixMap.put(ENROUTE, Arrays.asList("2014", "2149"));
+        prefixMap.put(JCB_16, Arrays.asList("3088", "3096", "3112", "3158", "3337", "3528"));
+        prefixMap.put(JCB_15, Arrays.asList("2100", "1800"));
+        prefixMap.put(VOYAGER,  Collections.singletonList("8699"));
 
+    }
 
     public static LabelEnum getLabel(String label){
         final String labelFilter = label.toUpperCase();
@@ -67,7 +48,7 @@ public enum LabelEnum {
     }
 
     public static List<String> getPrefixList(LabelEnum label){
-        return getMapLabel().get(label);
+        return prefixMap.get(label);
     }
 
 }
